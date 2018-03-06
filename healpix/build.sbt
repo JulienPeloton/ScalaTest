d6f0d638c6ac0037a93b9dcd323e1115e5c88697
Julien <peloton@lal.in2p3.fr>
2018-03-06 15:04:34 +0100

    Add libs

--- healpix/build.sbt
+++ healpix/build.sbt
@@ -12,6 +12,9 @@ lazy val root = (project in file(".")).
    coverageHighlighting := true,
    coverageMinimum := 70,
    publishArtifact in Test := false,
+   // fork in run := true,
+   // javaOptions += "-Djava.library.path=/Users/julien/Documents/workspace/otrepos/jep/build/lib.macosx-10.7-x86_64-3.6/",
+   // javaOptions += "-Djava.library.path=./lib",
    libraryDependencies ++= Seq(
      "org.apache.spark" %% "spark-core" % "2.1.0",
      scalaTest % Test
--- healpix/run.sh
+++ healpix/run.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/Users/julien/anaconda3/lib/python3.6/site-packages/jep/
+# export DYLD_FALLBACK_LIBRARY_PATH=$DYLD_FALLBACK_LIBRARY_PATH:/Users/julien/anaconda3/lib/python3.6/site-packages/jep/
+#export LD_PRELOAD=~/anaconda3/lib/libpython3.6m.dylib
 # Package it
 sbt package
 
+HEALPIX=lib/jhealpix.jar
+JEP=lib/jep-3.7.1.jar
+
 # Run it!
-spark-submit --master local[*] --jars lib/jhealpix.jar target/scala-2.11/healpix_2.11-0.1.0.jar
+spark-submit \
+  --master local[*] \
+  --jars ${HEALPIX},${JEP} \
+  target/scala-2.11/healpix_2.11-0.1.0.jar
